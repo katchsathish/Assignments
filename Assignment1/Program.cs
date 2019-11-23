1d77940c65f727b7ab9a5174ddc9fece212426d1
sathishkumar <maiil2sathish@gmail.com>
2019-11-23 12:05:18 +0530

    Add project files.

---
+++ Assignment1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class Program
+    {/*Assignment 1:
+        Write a program that prints the numbers from 1 to 100. 
+        But for multiples of three print "Fizz" instead of the number and for the
+        multiples of five print "Buzz".
+        For numbers which are multiples of both
+        three and five print "FizzBuzz".*/
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                int divBy3, divBy5;
+                divBy3 = i % 3;
+                divBy5 = i % 5;
+                if (divBy3 == 0 && divBy5 == 0)
+                    Console.WriteLine("FizzBuzz");
+                else if (divBy3 == 0)
+                    Console.WriteLine("Fizz");
+                else if (divBy5 == 0)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(i);
+
+            }
+
+
+        }
+    }
+}
---
+++ Assignment2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Assignment2
+{
+    class Program
+    {
+        /*Assignment 2:
+        Swap 2 nos with out using a third variable or any pre defined function
+        Swap(int x,int y)*/
+
+        static void Main(string[] args)
+        {
+            int x = 1, y = 2;
+            Console.WriteLine("Before Swapping {X}= " + x +" {Y}= "+y);
+            x = x + y;
+            y = x - y;
+            x = x - y;
+            Console.WriteLine("After Swapping {X}= " + x + " {Y}= " + y);
+        }
+            
+       
+          
+
+       
+
+    }
+}
